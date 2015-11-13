cc2ef288a31fbcd17dfd82375d7bf209fed23299
SergeyRy <SergeyRy@interlink-ua.com>
2015-11-13 18:06:42 +0200

    no message

---
+++ MyLunchMaster/MealService.h
@@ -0,0 +1,13 @@
+//
+//  MealService.h
+//  MyLunchMaster
+//
+//  Created by Employee on 13.11.15.
+//  Copyright © 2015 Employee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MealService : NSObject
+
+@end
---
+++ MyLunchMaster/MealService.m
@@ -0,0 +1,13 @@
+//
+//  MealService.m
+//  MyLunchMaster
+//
+//  Created by Employee on 13.11.15.
+//  Copyright © 2015 Employee. All rights reserved.
+//
+
+#import "MealService.h"
+
+@implementation MealService
+
+@end
